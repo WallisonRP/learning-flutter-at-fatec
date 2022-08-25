5d6e28492ad3c509a02eee12e6a371122406edce
Wallyz <80826888+WallisonRP@users.noreply.github.com>
2022-08-25 17:04:01 -0300

    ending desafio_04

--- desafio_04/lib/view/componentePrincipal.dart
+++ desafio_04/lib/view/componentePrincipal.dart
@@ -1,38 +1,51 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
 
 class ComponentePrincipal extends StatelessWidget {
-  const ComponentePrincipal({Key? key}) : super(key: key);
+  final IconData icone;
+  final String titulo;
+  final String corpo;
+
+  const ComponentePrincipal(
+      {Key? key,
+      required this.icone,
+      required this.titulo,
+      required this.corpo})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.lightBlueAccent,
       // height: 300,
-      margin: EdgeInsets.all(16),
+      margin: EdgeInsets.all(10),
       child: Column(
         children: [
           Container(
             padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
             child: Row(
-              children: [Icon(Icons.house), Text("Titulo")],
+              children: [Icon(icone), Text(titulo)],
             ),
           ),
           Container(
             color: Colors.grey,
-            padding: EdgeInsets.fromLTRB(10, 32, 10, 32),
+            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
             child: Row(
-              children: [
-                Flexible(
-                    child: Text(
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."))
-              ],
+              children: [Flexible(child: Text(corpo))],
             ),
           ),
           Container(
             padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
-            child: Row(
-              children: [Text("Explore"), Icon(Icons.arrow_right_sharp)],
-            ),
+            child: Row(children: [
+              Padding(
+                padding: EdgeInsets.only(right: 5),
+                child: Text("Explore"),
+              ),
+              Icon(
+                Icons.east,
+                size: 20,
+              ),
+            ]),
           ),
         ],
       ),
--- desafio_04/lib/view/telaPrincipal.dart
+++ desafio_04/lib/view/telaPrincipal.dart
@@ -2,7 +2,8 @@ import 'package:desafio_04/view/componentePrincipal.dart';
 import 'package:flutter/material.dart';
 
 class TelaPrincipal extends StatelessWidget {
-  const TelaPrincipal({Key? key}) : super(key: key);
+  String corpo =
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.";
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +12,36 @@ class TelaPrincipal extends StatelessWidget {
         title: Text("Lorem Ipsum"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [ComponentePrincipal()],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ComponentePrincipal(
+              icone: Icons.house,
+              titulo: "Titulo",
+              corpo: corpo,
+            ),
+            ComponentePrincipal(
+              icone: Icons.house,
+              titulo: "Titulo",
+              corpo: corpo,
+            ),
+            ComponentePrincipal(
+              icone: Icons.house,
+              titulo: "Titulo",
+              corpo: corpo,
+            ),
+            ComponentePrincipal(
+              icone: Icons.house,
+              titulo: "Titulo",
+              corpo: corpo,
+            ),
+            ComponentePrincipal(
+              icone: Icons.house,
+              titulo: "Titulo",
+              corpo: corpo,
+            ),
+          ],
+        ),
       ),
     );
   }
