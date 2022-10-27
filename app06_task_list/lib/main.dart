b5b8c7df3968ada824b63cf512238a1a0340a4f3
WallisonRP <wallison0076@gmail.com>
2022-10-27 18:18:46 -0300

    configuração do dotenv

--- app06_task_list/lib/main.dart
+++ app06_task_list/lib/main.dart
@@ -1,25 +1,20 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:app06_task_list/pages/login_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'firebase_options.dart';
 
 void main() async {
-  //Inicializar firebase
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-);
-  runApp(
-    MaterialApp(
-      title: 'Task List',
-      debugShowCheckedModeBanner: false,
-      initialRoute: 'login',
-      routes: {
-        'login' : ((context) => LoginPage())
-      },
-    )
-  );
-}
+  await dotenv.load(fileName: '.env');
 
+  runApp(MaterialApp(
+    title: 'Task List',
+    debugShowCheckedModeBanner: false,
+    initialRoute: 'login',
+    routes: {'login': ((context) => LoginPage())},
+  ));
+}
