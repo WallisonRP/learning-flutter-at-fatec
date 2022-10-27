d855e4e0d1d7c2f5c17be34a24d3831d119e90f3
WallisonRP <wallison0076@gmail.com>
2022-10-27 09:00:25 -0300

    construcao da interface e integracao com o firebase

---
+++ app06_task_list/lib/controller/login_controller.dart
@@ -0,0 +1,15 @@
+class LoginController {
+
+  void criarConta(context, String nome, String email, String senha) {
+
+  }
+
+  void login(context, String email, String senha) {
+
+  }
+
+  void esqueceuSenha(String email) {
+
+  }
+  
+}
